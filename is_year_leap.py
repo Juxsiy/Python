0b9b6c9822c2a6c39275996992c19006900df196
Juxsiy <juxsiy@gmail.com>
2022-03-30 17:56:54 +0300

    add is_year_leap

---
+++ is_year_leap.py
@@ -0,0 +1,12 @@
+def is_year_leap(year):
+    if (year % 4 == 0) and (year % 100 != 0) or (year % 400 == 0):
+        return True
+    else:
+        return False
+
+while True:
+    try:
+        year = int(input())
+        print(is_year_leap(year))
+    except ValueError:
+        print("Error. Please enter an integer")
